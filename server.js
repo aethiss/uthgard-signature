849726b79d4a9d1d354849b44b952daa74a5b326
Enzo D'Onofrio <aethiss@MacBook-Pro-di-Enzo.local>
2017-02-18 12:31:24 +0100

    deploy missed files

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 var express = require('express')
 var app = express()
+var request = require('request')
+
+// set render Engine
+app.set('view engine', 'ejs');
+app.use('/static', express.static('src'))
 
 app.get('/', function (req, res) {
-
-  var request = require('request');
-  request('https://uthgard.org/herald/api/player/Alita', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body) // Show the HTML for the Google homepage.
-    }
-  })
-
-  res.send('Hello World!')
+  res.send('Please use UthgardSignature/player/YOURNICK')
 })
 
-app.listen(3000, function () {
+app.get('/player/:nick', function (req, res) {
+  request('https://uthgard.org/herald/api/player/' + req.params.nick, function (error, response, body) {
+    if (response.statusCode === 404) {
+      res.send('Nick Name NOT FOUND')
+    }
+    if (!error && response.statusCode == 200) {
+      // res.send(body)
+      console.log(body)
+      res.render('signatures/standard', JSON.parse(body));
+    }
+  })
+})
+
+app.listen(80, function () {
   console.log('Uthgard Signature Server running on port 3000')
 })
