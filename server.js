da02e55fe808efc84c5cf12fda69b16e864d0559
Enzo D'Onofrio <aethiss@MacBook-Pro-di-Enzo.local>
2017-02-18 12:36:02 +0100

    8080 port

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get('/player/:nick', function (req, res) {
   })
 })
 
-app.listen(3000, function () {
-  console.log('Uthgard Signature Server running on port 3000')
+app.listen(8080, function () {
+  console.log('Uthgard Signature Server running on port 8080')
 })
