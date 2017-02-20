43adefd1717cc4c985078c7107d65385189b06db
Enzo D'Onofrio <aethiss@MacBook-Pro-di-Enzo.local>
2017-02-20 16:12:51 +0000

    add heroku standart port

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get('/player/:nick', function (req, res) {
   })
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Uthgard Signature Server running on port 3000')
 })
